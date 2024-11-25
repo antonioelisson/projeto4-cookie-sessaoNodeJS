479425bd8dc7e685ff2d4d3d2db62ef5c8458894
Elisson Antonio da Silva <123256274+antonioelisson@users.noreply.github.com>
2024-11-25 19:53:11 -0300

    Update index.js
    editado o modo importação import para require

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import session from "express-session";
-import cookieParser from "cookie-parser";
+const express = require('express');
+const session = ('express-session');
+const cookieParser = ('cookie-parser');
 
 const app = express();
 
